a69a70a824f33cd2efb462d1387d69ac760cc9f0
Josh Batchelor <josh_batch@live.com.au>
2021-06-26 19:32:32 +1000

    Adds new socket types for refactor

--- typings/socket.d.ts
+++ typings/socket.d.ts
@@ -1,28 +1,36 @@
-type JoinMessage = { username: string };
-type DisconnectMessage = {};
-type GameStateMessage = {
+// Client
+type ClientJoin = { username: string };
+type ClientDisconnect = {};
+type ClientLocation = {
+  endX: number;
+  endY: number;
+};
+type ClientDraw = {
+  imageData: string; // Data URL of image to append to Canvas
+};
+
+// Server
+type ServerWelcome = {
+  id: string;
+  color: string;
+}
+type ServerUpdateCursors = {
   t: number;
   serverFps: number;
   cursors: Array<PlayerState>;
-  canvasBuffer: string;
 };
-type InputMessage = {
-  startX: number;
-  startY: number;
-  endX: number;
-  endY: number;
-  mouseDown: boolean;
-};
-type DrawImageMessage = {
-  imageData: string; // Data URL of image to append to Canvas
-};
-type GameOverMessage = {};
+type ServerUpdateBackground = {
+  backgroundImage: string; // Data URL of the new background for clients
+}
 
 type SocketEvents = {
-  join: JoinMessage;
-  disconnect: DisconnectMessage;
-  'game-state': GameStateMessage;
-  input: InputMessage;
-  'draw-image': DrawImageMessage;
-  'game-over': GameOverMessage;
+  // Sent by client
+  'client-join': ClientJoin;
+  'client-disconnect': ClientDisconnect;
+  'client-location': ClientLocation;
+  'client-draw': ClientDraw;
+  // Sent by Server
+  'server-welcome': ServerWelcome;
+  'server-update-cursors': ServerUpdateCursors;
+  'server-update-background': ServerUpdateBackground;
 };
